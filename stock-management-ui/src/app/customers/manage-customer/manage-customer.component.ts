ad79035791a9b37b498b9a59d7003db7eaf2dab2
hd9719 <hd9719@gmail.com>
2020-03-01 22:03:45 +0530

    service for api call

--- stock-management-ui/src/app/customers/manage-customer/manage-customer.component.ts
+++ stock-management-ui/src/app/customers/manage-customer/manage-customer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '../service/customer.service';
 
 @Component({
   selector: 'app-manage-customer',
@@ -7,14 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageCustomerComponent implements OnInit {
   supplier:any={};
-  constructor() { }
+  
+  constructor(private customerService : CustomerService) { }
   
   ngOnInit() {
   }
   onSaveOrUpdate(){
     
     console.log(this.supplier);
-    
+    this.customerService.saveOrUpdateSupplier(this.supplier).subscribe((data)=>{
+      console.log('success');
+      
+    },(error)=>{
+      console.log(error);
+      
+    })
         
   }
 }
