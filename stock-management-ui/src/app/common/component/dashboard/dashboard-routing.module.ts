85f2b551b3f22810461151559e19d96468bdf6fb
ketul <mayankdudhatra01@gmail.com>
2020-03-21 00:21:22 +0530

    conflict resolved

--- stock-management-ui/src/app/common/component/dashboard/dashboard-routing.module.ts
+++ stock-management-ui/src/app/common/component/dashboard/dashboard-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+import { LoginComponent } from 'src/app/login/login.component';
 
 
 const routes: Routes = [
