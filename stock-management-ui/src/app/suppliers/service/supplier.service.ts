a1b3da5dc4c364fea082b7dd86ce0ff87506ce2d
kadam patel <kbpatel@mittals.net>
2020-03-22 12:04:22 +0530

    [inward stock services]

--- stock-management-ui/src/app/suppliers/service/supplier.service.ts
+++ stock-management-ui/src/app/suppliers/service/supplier.service.ts
@@ -1,31 +1,31 @@
 import { Injectable } from '@angular/core';
-import { environment} from '../../../environments/environment';
+import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-const BASE_URL :string = environment.BASE_URL;
+const BASE_URL: string = environment.BASE_URL;
 @Injectable({
-  providedIn: 'root'
+   providedIn: 'root'
 })
 export class SupplierService {
 
-  
-  constructor(private http: HttpClient) { }
-   saveOrUpdateSupplier(supplier:any):Observable<any>{
-      return this.http.post(`${BASE_URL}suppliers`,supplier);
+
+   constructor(private http: HttpClient) { }
+   saveOrUpdateSupplier(supplier: any): Observable<any> {
+      return this.http.post(`${BASE_URL}suppliers`, supplier);
    }
-   getSuppliers():Observable<any []>{
-      return this.http.get<any []>(`${BASE_URL}suppliers/all`);
+   getSuppliers(): Observable<any[]> {
+      return this.http.get<any[]>(`${BASE_URL}suppliers/all`);
    }
-   getSupplier(id:number):Observable<any []>{
-     console.log(id);
-      return this.http.get<any []>(`${BASE_URL}suppliers/${id}`);
+   getSupplier(id: number): Observable<any[]> {
+      console.log(id);
+      return this.http.get<any[]>(`${BASE_URL}suppliers/${id}`);
    }
-   deleteSupplier(id:any):Observable<any>{
-    var supplier = { 
-      SupplierID:id, 
-      IsActive:0 
-   };
-      return this.http.post<any>(`${BASE_URL}suppliers`,supplier);
+   deleteSupplier(id: any): Observable<any> {
+      var supplier = {
+         SupplierID: id,
+         IsActive: 0
+      };
+      return this.http.post<any>(`${BASE_URL}suppliers`, supplier);
    }
 }
