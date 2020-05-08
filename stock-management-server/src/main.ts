84110bd1749be65508ef4a4414a66fde4f12330f
kadam patel <kbpatel@mittals.net>
2020-05-08 18:49:56 +0530

    [message : some changes]

--- stock-management-server/src/main.ts
+++ stock-management-server/src/main.ts
@@ -8,6 +8,6 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.enableCors();
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
